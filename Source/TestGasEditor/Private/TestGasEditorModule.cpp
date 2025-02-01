fd54845b78a260b9ec110c1bdbf195e4056ed46b
lovebird <lovebirdsx@gmail.com>
2025-02-01 21:10:07 +0800

    加入编辑器模式下更新actor的测试

--- Source/TestGasEditor/Private/TestGasEditorModule.cpp
+++ Source/TestGasEditor/Private/TestGasEditorModule.cpp
@@ -5,8 +5,10 @@ IMPLEMENT_MODULE(FTestGasEditor, TestGasEditor);
 
 void FTestGasEditor::StartupModule()
 {
+	UE_LOG(LogTemp, Warning, TEXT("TestGasEditor module has started!"));
 }
 
 void FTestGasEditor::ShutdownModule()
 {
+	UE_LOG(LogTemp, Warning, TEXT("TestGasEditor module has shut down!"));
 }
